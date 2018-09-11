555ac27aadf9349ada89bf776bc88099db8e74e0
jtrtj <jt.r@live.com>
2018-09-11 16:48:27 -0600

    pass predatabasevote spec again with new attributes

--- app/services/pre_database_vote.rb
+++ app/services/pre_database_vote.rb
@@ -23,6 +23,58 @@ class PreDatabaseVote
     end
   end
 
+  def result
+    data[:result]
+  end
+
+  def democrat_yes
+    data[:democratic][:yes]
+  end
+
+  def democrat_no
+    data[:democratic][:no]
+  end
+
+  def democrat_not_voting
+    data[:democratic][:not_voting]
+  end
+
+  def republican_yes
+    data[:republican][:yes]
+  end
+
+  def republican_no
+    data[:republican][:no]
+  end
+
+  def republican_not_voting
+    data[:republican][:not_voting]
+  end
+
+  def independent_yes
+    data[:independent][:yes]
+  end
+
+  def independent_no
+    data[:independent][:no]
+  end
+
+  def independent_not_voting
+    data[:independent][:not_voting]
+  end
+
+  def total_yes
+    data[:total][:yes]
+  end
+
+  def total_no
+    data[:total][:no]
+  end
+
+  def total_not_voting
+    data[:total][:not_voting]
+  end
+  
   private
   attr_reader :data
 end
--- spec/services/pre_database_vote_spec.rb
+++ spec/services/pre_database_vote_spec.rb
@@ -5,12 +5,26 @@ describe PreDatabaseVote do
     it 'creates attributes to be saved in the database from a ProPublica vote hash' do
       file = File.read('spec/fixtures/mock_nomination_vote.json')
       data = JSON.parse(file, symbolize_names: true)
+      
       pre_database_vote = PreDatabaseVote.new(data)
 
       expect(pre_database_vote.number).to eq(data[:nomination][:number])
       expect(pre_database_vote.question).to eq(data[:question])
       expect(pre_database_vote.description).to eq(data[:description])
       expect(pre_database_vote.type_of).to eq(data.keys[9].to_s)
+      expect(pre_database_vote.result).to eq(data[:result])
+      expect(pre_database_vote.democrat_yes).to eq(data[:democratic][:yes])
+      expect(pre_database_vote.democrat_no).to eq(data[:democratic][:no])
+      expect(pre_database_vote.democrat_not_voting).to eq(data[:democratic][:not_voting])
+      expect(pre_database_vote.republican_yes).to eq(data[:republican][:yes])
+      expect(pre_database_vote.republican_no).to eq(data[:republican][:no])
+      expect(pre_database_vote.republican_not_voting).to eq(data[:republican][:not_voting])
+      expect(pre_database_vote.independent_yes).to eq(data[:independent][:yes])
+      expect(pre_database_vote.independent_no).to eq(data[:independent][:no])
+      expect(pre_database_vote.independent_not_voting).to eq(data[:independent][:not_voting])
+      expect(pre_database_vote.total_yes).to eq(data[:total][:yes])
+      expect(pre_database_vote.total_no).to eq(data[:total][:no])
+      expect(pre_database_vote.total_not_voting).to eq(data[:total][:not_voting])
     end
 
     it 'can create a vote for either a nomination or a bill' do
