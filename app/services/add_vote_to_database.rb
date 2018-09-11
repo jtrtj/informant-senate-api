ff5f61f4d2a0f3f4dd84e5eceadf3917439c447b
jtrtj <jt.r@live.com>
2018-09-11 16:55:07 -0600

    everything back to green after adding all the results columns to vote

--- app/services/add_vote_to_database.rb
+++ app/services/add_vote_to_database.rb
@@ -3,6 +3,20 @@ class AddVoteToDatabase < SimpleDelegator
     Vote.create(number: number, 
                 question: question,
                 description: description,
-                type_of: type_of)
+                type_of: type_of,
+                result: result,
+                democrat_yes: democrat_yes,
+                democrat_no: democrat_no,
+                democrat_not_voting: democrat_not_voting,
+                republican_yes: republican_yes,
+                republican_no: republican_no,
+                republican_not_voting: republican_not_voting,
+                independent_yes: independent_yes,
+                independent_no: independent_no,
+                independent_not_voting: independent_not_voting,
+                total_yes: total_yes,
+                total_no: total_no,
+                total_not_voting: total_not_voting
+                )
   end
 end
