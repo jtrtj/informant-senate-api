da8e4df1f7dbb171497c606ab37bcfbe204dd4a5
jtrtj <jt.r@live.com>
2018-09-11 17:55:55 -0600

    start test for SavePropublicaVotesResponseToDatabase service

---
+++ spec/services/save_propublica_votes_response_to_database_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe SavePropublicaVotesResponseToDatabase do
+  context 'class methods' do
+    it '.save_recent_votes' do
+      expect { SavePropublicaVotesResponseToDatabase.save_recent_votes }.to
+        change { Vote.count }.by(20)
+    end
+  end
+end
