5853a7b27f52f867788dd59433ce2fc4f496541d
jtrtj <jt.r@live.com>
2018-09-11 16:25:00 -0600

    add validations for new vote columns

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,20 @@ class Vote < ApplicationRecord
   validates_presence_of :number,
                         :question,
                         :description,
-                        :type_of
+                        :type_of,
+                        :result,
+                        :democrat_yes,
+                        :democrat_no,
+                        :democrat_not_voting,
+                        :republican_yes,
+                        :republican_no,
+                        :republican_not_voting,
+                        :independent_yes,
+                        :independent_no,
+                        :independent_not_voting,
+                        :total_yes,
+                        :total_no,
+                        :total_not_voting
+
   enum type_of: [:bill, :nomination]
-  has_one :result
 end
