63fedb831e6cdfa460353e1f95a9355ba3ea203a
jtrtj <jt.r@live.com>
2018-09-10 21:18:19 -0600

    add module for test to parse response body and add @votes to index

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,2 +1,6 @@
-class VotesController < ApplicationController
+class Api::V1::VotesController < ApplicationController
+  def index
+    @votes = Vote.all
+    json_response(@votes)
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
 
-# [...]
+
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -19,25 +19,21 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# [...]
-RSpec.configure do |config|
-  # [...]
-  # add `FactoryBot` methods
-  config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
 
-  # start the transaction strategy as examples are run
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
     end
   end
-  # [...]
+  
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -53,7 +49,7 @@ end
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -65,7 +61,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include RequestSpecHelper, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
