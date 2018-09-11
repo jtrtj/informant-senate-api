e3d858c7e26366e32fc93b03986785a377e4016b
jtrtj <jt.r@live.com>
2018-09-10 21:45:32 -0600

    add expectations for keys in test

--- spec/api/v1/requests/votes_spec.rb
+++ spec/api/v1/requests/votes_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe 'Votes API', type: :request do
   context 'GET /api/v1/votes' do
     before { get '/api/v1/votes' }
     
-    it 'returns votes' do
-      
-      binding.pry
-      
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
-    end
-
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
+
+    it 'returns votes' do
+      expect(json["data"].first["attributes"]).to have_key("number")
+      expect(json["data"].first["attributes"]).to have_key("question")
+      expect(json["data"].first["attributes"]).to have_key("description")
+      expect(json["data"].first["attributes"]).to have_key("type_of")
+      expect(json["data"].size).to eq(10)
+    end
+
   end
 end
