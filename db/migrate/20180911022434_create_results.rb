6774be6008c2603c879c6aebfe138b01cf3b918e
jtrtj <jt.r@live.com>
2018-09-11 16:02:21 -0600

    add columns to the results table

--- db/migrate/20180911022434_create_results.rb
+++ db/migrate/20180911022434_create_results.rb
@@ -3,6 +3,18 @@ class CreateResults < ActiveRecord::Migration[5.1]
     create_table :results do |t|
       t.references :vote, foreign_key: true
       t.string :result
+      t.integer :democrat_yes
+      t.integer :democrat_no
+      t.integer :democrat_not_voting
+      t.integer :republican_yes
+      t.integer :republican_no
+      t.integer :republican_not_voting
+      t.integer :independent_yes
+      t.integer :independent_no
+      t.integer :independent_not_voting
+      t.integer :total_yes
+      t.integer :total_no
+      t.integer :total_not_voting
 
       t.timestamps
     end
