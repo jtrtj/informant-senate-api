1ddb98359acf1e04a836d7598c9101c35c6b969e
jtrtj <jt.r@live.com>
2018-09-11 12:15:31 -0600

    change ApiVote to PreDatabaseVote

--- spec/services/add_vote_to_database_spec.rb
+++ spec/services/add_vote_to_database_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe AddVoteToDatabase do
   context 'class methods' do
     it '.add_vote(vote) - takes Vote object and adds it to the database' do
-      vote = JsonVote.new(data)
+      vote = PreDatabaseVote.new(data)
       
       expect { AddVoteToDatabase.add_vote(vote) }.to change { Vote.count }.by(1)
     end
