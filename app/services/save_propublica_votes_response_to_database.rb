6ef234090679be29b49ea47262dbcc566fa2f2cd
jtrtj <jt.r@live.com>
2018-09-11 21:21:54 -0600

    start building propublica connection and the methds that will use it on SavePropublicaVotesResponseToDatabase

--- app/services/save_propublica_votes_response_to_database.rb
+++ app/services/save_propublica_votes_response_to_database.rb
@@ -1,2 +1,21 @@
 class SavePropublicaVotesResponseToDatabase
+  def self.save_recent_votes
+    get_response_body_results.each do | vote_data |
+      AddVoteToDatabase.new(PreDatabaseVote.new(vote_data)).add_vote
+    end
+  end
+
+  private
+
+  def self.get_response_body_results
+    JSON.parse(make_request.body[:results], symbolize_names: true)
+  end
+
+  def self.make_request
+    conn.get('/congress/v1/senate/votes/recent.json')
+  end
+
+  def self.conn
+    ProPublicaConnection.conn
+  end
 end
--- spec/services/save_propublica_votes_response_to_database_spec.rb
+++ spec/services/save_propublica_votes_response_to_database_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe SavePropublicaVotesResponseToDatabase do
   context 'class methods' do
     it '.save_recent_votes' do
-      expect { SavePropublicaVotesResponseToDatabase.save_recent_votes }.to
-        change { Vote.count }.by(20)
+      expect { SavePropublicaVotesResponseToDatabase.save_recent_votes }.to change { Vote.count }.by(20)
     end
   end
 end
