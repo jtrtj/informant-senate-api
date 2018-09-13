f905b4f75052c0bcd2f789943cecdb77e955cfcb
jtrtj <jt.r@live.com>
2018-09-13 11:10:26 -0600

    make method that needs ArticlesFromNewsApi

--- app/services/save_news_api_response_to_articles.rb
+++ app/services/save_news_api_response_to_articles.rb
@@ -1,13 +1,19 @@
 class SaveNewsApiResponseToArticles < SimpleDelegator
   def save_news_to_article
     pre_database_articles.each do | article |
-      Article.create(vote: self,
-                    title: article.title,
-                    description: article.description,
-                    url: article.url,
-                    image_url: article.image_url,
-                    source: article.source
+      Article.create(vote:        self,
+                     title:       article.title,
+                     description: article.description,
+                     url:         article.url,
+                     image_url:   article.image_url,
+                     source:      article.source
                     )
     end
+  end
+
+  private
+
+  def pre_database_articles
+    ArticlesFromNewsApi.fetch(description)
   end
 end
