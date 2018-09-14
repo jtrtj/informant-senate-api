17811fe49037de3cc0622790a8f17966193e2be5
jtrtj <jt.r@live.com>
2018-09-13 22:02:32 -0600

    add Article model - all tests green

--- app/services/save_news_api_response_to_articles.rb
+++ app/services/save_news_api_response_to_articles.rb
@@ -1,7 +1,7 @@
 class SaveNewsApiResponseToArticles < SimpleDelegator
   def save_news_to_article
     pre_database_articles.each do | article |
-      Article.create(vote:        self,
+      Article.create(vote_id:     self.id,
                      title:       article.title,
                      description: article.description,
                      url:         article.url,
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Article do
   context 'validations' do
-    it { should validate_presence_of(:vote) }
+    it { should validate_presence_of(:vote_id) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:url) }
     it { should validate_presence_of(:image_url) }
