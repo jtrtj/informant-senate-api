f442ec87830c0286c424a7a05ffbe6e5e8173be6
jtrtj <jt.r@live.com>
2018-09-16 16:19:14 -0600

    refactor

--- app/serializers/vote_blueprint.rb
+++ app/serializers/vote_blueprint.rb
@@ -4,13 +4,3 @@ class VoteBlueprint < Blueprinter::Base
   field :created_at, datetime_format: "%m/%d/%Y"
   association :articles, blueprint: ArticleBlueprint
 end
-
-# class UserBlueprint < Blueprinter::Base
-#   identifier :uuid
-#   field :email, name: :login
-
-#   view :normal do
-#     fields :first_name, :last_name
-#     association :projects, blueprint: ProjectBlueprint
-#   end
-# end
--- app/services/save_propublica_votes_response_to_database.rb
+++ app/services/save_propublica_votes_response_to_database.rb
@@ -1,11 +1,17 @@
 class SavePropublicaVotesResponseToDatabase
   def self.save_recent_votes
-    get_response_body_results.each do | vote_data |
-      AddVoteToDatabase.new(PreDatabaseVote.new(vote_data)).add_vote
+    pre_database_votes_from_response.each do | pre_database_vote |
+      AddVoteToDatabase.new(pre_database_vote).add_vote
     end
   end
 
   private
+
+  def self.pre_database_votes_from_response
+    get_response_body_results.map do | vote_data |
+      PreDatabaseVote.new(vote_data)
+    end
+  end
 
   def self.get_response_body_results
     JSON.parse(make_request.body, symbolize_names: true)[:results][:votes]
