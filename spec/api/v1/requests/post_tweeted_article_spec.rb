e61a8f15ca75ce5f5754db6a7ae3d4f6009b6e13
jtrtj <jt.r@live.com>
2018-09-17 10:48:40 -0600

    write test for tweeted articles post request

---
+++ spec/api/v1/requests/post_tweeted_article_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Votes Api', type: :request do
+  let!(:vote) { create(:vote)}
+  let!(:article) { create(:article, vote: vote)}
+
+  context 'POST /api/v1/tweeted-articles' do
+    it 'creates a tweeted article in the db from the provided params' do
+      uid = '03498573'
+      article_id = article.id
+
+      post "/api/v1/tweeted-articles?uid=#{uid}&article=#{article_id}"
+
+      expect(TweetedArticle.last.uid).to eq(uid)
+      expect(TweetedArticle.last.article).to eq(article)
+    end
+  end
+end
